99e6f8d13c56b6c5b0a6f902380c6f1369b16b6c
BeeProductive <beeproductiveapphelp@gmail.com>
2025-04-21 18:14:03 -0700

    fixing syntax and old code before crash

--- src/IOSCodeSnippets/pinViewController.swift
+++ src/IOSCodeSnippets/pinViewController.swift
@@ -1,21 +1,22 @@
 import UIKit
-import FirebaseAuth
 import FirebaseFirestore
+import FirebaseAuth
 
-class pinViewController: UIViewController, UITableViewDataSource {
-	
-	let db = Firestore.firestore()
-	
-	@IBOutlet weak var newPinButton: UIButton!
 
+class pinViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let db = Firestore.firestore()
+    
+    @IBOutlet weak var newPinButton: UIButton!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return testData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "pinCell", for: indexPath)
-                
-                cell.textLabel?.text = testData[indexPath.row]
+        
+        cell.textLabel?.text = testData[indexPath.row]
         cell.textLabel?.textColor = .white
         cell.textLabel?.backgroundColor = .systemBlue
         
@@ -25,12 +26,12 @@ class pinViewController: UIViewController, UITableViewDataSource {
         
         return cell
     }
-
-	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
                    forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             guard let user = Auth.auth().currentUser else { return }
-
+// find pin to delete
             let pinToDelete = testData[indexPath.row]
 
             let userRef = db.collection("users").document(user.uid)
@@ -38,9 +39,9 @@ class pinViewController: UIViewController, UITableViewDataSource {
                 "userPins": FieldValue.arrayRemove([pinToDelete])
             ]) { error in
                 if let error = error {
-                    print("error in deletion\(error.localizedDescription)")
+                    print("\(error.localizedDescription)")
                 } else {
-                    print("deleted pin")
+                    print("deleted PIN" success")
                     
                     self.testData.remove(at: indexPath.row)
                     tableView.deleteRows(at: [indexPath], with: .fade)
@@ -48,45 +49,46 @@ class pinViewController: UIViewController, UITableViewDataSource {
             }
         }
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return 100
-        }
+        return 100
+    }
     
-
-    
-    @IBOutlet weak var pinTableView: UITableView!
-
-@IBAction func generatePin(_ sender: Any) {
-guard let user = Auth.auth().currentUser else {
+   
+    @IBAction func generatePin(_ sender: Any) {
+        
+        guard let user = Auth.auth().currentUser else {
             return
         }
         
         let allowedCharacters = ["0", "1", "2", "3", "7", "8", "9", "A", "C", "D"]
         var newPin = ""
-	for _ in 0..<4 {
+
+        for _ in 0..<4 {
             if let randomChar = allowedCharacters.randomElement() {
                 newPin.append(randomChar)
             }
         }
-
-	  testData.append(newPin)
+        
+        testData.append(newPin)
         pinTableView.reloadData()
         
         let userRef = db.collection("users").document(user.uid)
-
-	 userRef.updateData([
+          
+           userRef.updateData([
                "userPins": FieldValue.arrayUnion([newPin])
            ]) { error in
                if let error = error {
                    print("\(error.localizedDescription)")
                } else {
-                   print("New PIN added")
+                   print("Added ne PIN")
                }
            }
-}
-	func loadUserPins() {
+        
+    }
+    func loadUserPins() {
         guard let user = Auth.auth().currentUser else {
-            print("no user auth")
+            print("Auth user not found")
             return
         }
 
@@ -94,41 +96,47 @@ guard let user = Auth.auth().currentUser else {
         
         userRef.getDocument { (document, error) in
             if let error = error {
-                print("error\(error.localizedDescription)")
+                print("\(error.localizedDescription)")
                 return
             }
             
             if let document = document, document.exists {
-                if let pins = document.data()?["userPins"] as? [String] {
+                if let pins = document.data()?["userPins"] as?[String] {
                     self.testData = pins
                     self.pinTableView.reloadData()
                 } else {
-                    print("userPins doesn't exist")
+                    print("array not present")
                 }
             } else {
-                print("document doesn't exist")
+                print("NA")
             }
         }
     }
     
+    @IBOutlet weak var pinTableView: UITableView!
+    
+    // placeholder below
+    var testData = [""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         pinTableView.delegate = self
+        pinTableView.delegate = self
+        
+        
         pinTableView.dataSource = self
-    pinTableView.backgroundColor = .white
-   
+        
+        pinTableView.backgroundColor = .white
         pinTableView.rowHeight = 100
-loadUserPins()
+        loadUserPins()
         
         pinTableView.reloadData()
-        
-        
+     
     }
-override func viewWillAppear(_ animated: Bool) {
-	super.viewWillAppear(animated)
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         loadUserPins()
-}
+    }
 
 
 }
