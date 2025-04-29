efa41438844cbd51a1b3a30033ade5ae4832d09f
BeeProductive <beeproductiveapphelp@gmail.com>
2025-04-28 18:17:49 -0700

    login view controller outlets

--- src/IOSCodeSnippets/loginViewController.swift
+++ src/IOSCodeSnippets/loginViewController.swift
@@ -4,4 +4,10 @@ import FirebaseAuth
 
 class loginViewController: UIViewController {
 
+  @IBOutlet weak var emailLog: UITextField!
+    @IBOutlet weak var passLog: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var goToSignUpButton: UIButton!
+
 }
