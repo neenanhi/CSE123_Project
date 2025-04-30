788e5282525b3de66be7f7bb44cbbf4e3169a1c1
BeeProductive <beeproductiveapphelp@gmail.com>
2025-04-29 18:30:14 -0700

    app delegate with firebase config

--- src/IOSCodeSnippets/AppDelegate.swift
+++ src/IOSCodeSnippets/AppDelegate.swift
@@ -10,6 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        
         
+        FirebaseApp.configure()
         return true
     }
 
@@ -29,5 +30,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
 
