2eca4f69872ea48d1179fd6942403dd3ad94b7d6
BeeProductive <beeproductiveapphelp@gmail.com>
2025-04-29 18:23:33 -0700

    adding old outlets and db ref

--- src/IOSCodeSnippets/logoutViewController.swift
+++ src/IOSCodeSnippets/logoutViewController.swift
@@ -3,6 +3,12 @@ import FirebaseAuth
 import UIKit
 
 class logoutViewController: UIViewController {
-
+	
+let db = Firestore.firestore()
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    
+    @IBOutlet weak var logoutButton: UIButton!
 
 }
