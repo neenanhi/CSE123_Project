54ea7cf5d3d6f5e59b90a6a56051e3f295e3f13b
BeeProductive <beeproductiveapphelp@gmail.com>
2025-04-29 18:28:05 -0700

    fixed old syntax

--- src/IOSCodeSnippets/logoutViewController.swift
+++ src/IOSCodeSnippets/logoutViewController.swift
@@ -3,18 +3,18 @@ import FirebaseAuth
 import UIKit
 
 class logoutViewController: UIViewController {
-	
-let db = Firestore.firestore()
+    
+    
+    let db = Firestore.firestore()
     
     @IBOutlet weak var usernameLabel: UILabel!
     
     
     @IBOutlet weak var logoutButton: UIButton!
-
-
-
-	override func viewDidLoad() {
-            super.viewDidLoad()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         fetchUsername()
         
@@ -22,7 +22,7 @@ let db = Firestore.firestore()
     
     func fetchUsername() {
             guard let user = Auth.auth().currentUser else {
-                usernameLabel.text = "no user logged in currently"
+                usernameLabel.text = "No current user"
                 return
             }
 
@@ -30,31 +30,35 @@ let db = Firestore.firestore()
             userDocRef.getDocument { (document, error) in
                 if let error = error {
                     print("\(error.localizedDescription)")
-                    self.usernameLabel.text = "can't load username"
+                    self.usernameLabel.text = "Can't load username"
                     return
                 }
 
                 if let document = document, document.exists {
                     let data = document.data()
-                    let username = data?["email"] as? String ?? "? user"
+                    let username = data?["email"] as? String ?? "User not known"
                     self.usernameLabel.text = "\(username)"
                 } else {
-                    self.usernameLabel.text = "Unable to locate user"
+                    self.usernameLabel.text = "Cannot find user"
                 }
             }
         }
-
-@IBAction func userLoggedOut(_ sender: Any) {
+    
+    
+    @IBAction func userLoggedOut(_ sender: Any) {
         
         do {
                 try Auth.auth().signOut()
                 
                 self.performSegue(withIdentifier: "logoutToLogin", sender: self)
             } catch let signOutError as NSError {
-                print("signout error: %@", signOutError)
-                let alert = UIAlertController(title: "failed to log user out", message: signOutError.localizedDescription, preferredStyle: .alert)
+                print("Signout error: %@", signOutError)
+                let alert = UIAlertController(title: "Failed to log out", message: signOutError.localizedDescription, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "", style: .default, handler: nil))
                 self.present(alert, animated: true)
             }
     }
+    
+    
 }
+
