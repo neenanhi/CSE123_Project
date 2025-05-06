47e350f771b9eb66d3fd5ee8a21e521bb8f5168d
BeeProductive <beeproductiveapphelp@gmail.com>
2025-05-05 17:22:53 -0700

    outlets and firestore

--- src/IOSCodeSnippets/keySetupViewController.swift
+++ src/IOSCodeSnippets/keySetupViewController.swift
@@ -4,5 +4,12 @@ import FirebaseAuth
 
 class keySetupViewController: UIViewController {
 
+	@IBOutlet weak var keyEntered: UITextField!
+    	@IBOutlet weak var setKeyButton: UIButton!
+    
+    	let db = Firestore.firestore()
+    	override func viewDidLoad() {
+        	super.viewDidLoad()
+    	}
 
 }
