bbeaff10bd801abff70fcb9ec9b4b431a9182843
BeeProductive <beeproductiveapphelp@gmail.com>
2025-05-05 17:26:32 -0700

    emergency pins outlets and setup

--- src/IOSCodeSnippets/tempPinViewController.swift
+++ src/IOSCodeSnippets/tempPinViewController.swift
@@ -4,6 +4,16 @@ import FirebaseAuth
 
 class tempPinViewController: UIViewController, UITableViewDataSource {
 
-
+	let db = Firestore.firestore()
+    	var listener: ListenerRegistration?
+   
+    	@IBOutlet weak var newPinButton2: UIButton!
+    
+    	@IBOutlet weak var pinTableView2: UITableView!
+    
+    	var accessKeyOverlay: UIView?
+    
+    
+   	 var testData2 = [""]
 
 }
