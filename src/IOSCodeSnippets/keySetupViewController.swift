4b998847daf08e5ca7b885952a1eb1ab32579d23
BeeProductive <beeproductiveapphelp@gmail.com>
2025-05-05 17:24:23 -0700

    key setup help

--- src/IOSCodeSnippets/keySetupViewController.swift
+++ src/IOSCodeSnippets/keySetupViewController.swift
@@ -3,25 +3,34 @@ import FirebaseFirestore
 import FirebaseAuth
 
 class keySetupViewController: UIViewController {
-
-	@IBOutlet weak var keyEntered: UITextField!
-    	@IBOutlet weak var setKeyButton: UIButton!
     
-    	let db = Firestore.firestore()
-    	override func viewDidLoad() {
-        	super.viewDidLoad()
-    	}
+    @IBOutlet weak var keyEntered: UITextField!
+    @IBOutlet weak var setKeyButton: UIButton!
+    
+    let db = Firestore.firestore()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
-
-	@IBAction func setKeyUser(_ sender: Any) {
-        	guard let user = Auth.auth().currentUser else {
-            		return
-        	}
-        	
-		guard let accessKey = keyEntered.text, !accessKey.isEmpty else {
-            		return
-        	}
-
-
-	}
+    @IBAction func setKeyUser(_ sender: Any) {
+        guard let user = Auth.auth().currentUser else {
+            return
+        }
+        guard let accessKey = keyEntered.text, !accessKey.isEmpty else {
+            return
+        }
+        
+        let userRef = db.collection("users").document(user.uid)
+    
+        
+        userRef.setData(["accessKey": accessKey], merge: true) { error in
+            if let error = error {
+                //
+            } else {
+                UserDefaults.standard.set(true, forKey: "didSetAccessKey")
+                UserDefaults.standard.synchronize()
+                self.dismiss(animated:true, completion: nil)
+            }
+        }
+    }
 }
