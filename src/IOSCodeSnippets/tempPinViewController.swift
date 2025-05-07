9cae44ba475db54d21063916ba5575c43535a8d1
BeeProductive <beeproductiveapphelp@gmail.com>
2025-05-06 17:27:24 -0700

    emergency pins table view setups

--- src/IOSCodeSnippets/tempPinViewController.swift
+++ src/IOSCodeSnippets/tempPinViewController.swift
@@ -16,4 +16,24 @@ class tempPinViewController: UIViewController, UITableViewDataSource {
     
    	 var testData2 = [""]
 
+	 func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        	return testData2.count
+    	}
+
+	 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pinCell2", for: indexPath)
+        cell.textLabel?.text = testData2[indexPath.row]
+        cell.textLabel?.textColor = .white
+      
+        cell.textLabel?.backgroundColor = UIColor(red: 151/255, green: 49/255, blue: 39/255, alpha: 1)
+        
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 40)
+        
+        cell.textLabel?.textAlignment = .center
+        
+        return cell
+    }
+
 }
