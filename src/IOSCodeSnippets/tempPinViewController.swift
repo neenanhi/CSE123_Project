fa43712212427dbd937c95168964bb4fbe5f275c
BeeProductive <beeproductiveapphelp@gmail.com>
2025-05-06 17:28:41 -0700

    obsecured older work keys

--- src/IOSCodeSnippets/tempPinViewController.swift
+++ src/IOSCodeSnippets/tempPinViewController.swift
@@ -35,5 +35,41 @@ class tempPinViewController: UIViewController, UITableViewDataSource {
         
         return cell
     }
+	
+func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    @IBAction func generateNewPin2(_ sender: Any) {
+        
+        guard let user = Auth.auth().currentUser else {
+            return
+        }
+        // test allow
+        let allowedCharacters2 = ["0", "1", "2", "3", "7", "8", "9", "A", "C", "D"]
+        var newPin2 = ""
+
+       // gen here
+        
+        testData2.append(newPin2)
+        pinTableView2.reloadData()
+        
+        let userRef = db.collection("users").document(user.uid)
+        
+        userRef.updateData([
+            "emergencyUserPins": FieldValue.arrayUnion([newPin2])
+        ]) { error in
+            if let error = error {
+            //
+            } else {
+                print("added new PIN.")
+            }
+        }
+    }
+
+	
+
+
+
 
 }
