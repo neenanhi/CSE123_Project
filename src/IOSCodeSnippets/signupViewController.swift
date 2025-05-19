251faf110556e9823b71d00afec5d6ec9c6e3f6b
BeeProductive <beeproductiveapphelp@gmail.com>
2025-05-19 14:22:20 -0700

    IBoutlets

--- src/IOSCodeSnippets/signupViewController.swift
+++ src/IOSCodeSnippets/signupViewController.swift
@@ -3,6 +3,19 @@ import FirebaseFirestore
 import FirebaseAuth
 
 class signupViewController: UIViewController {
-
+@IBOutlet weak var emailTxtField: UITextField!
+    
+    
+    @IBOutlet weak var pwdTxtField: UITextField!
+    
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    @IBOutlet weak var goToLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
 }
