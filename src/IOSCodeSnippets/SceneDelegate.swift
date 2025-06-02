17c68db12bf52c1c44066533cac50199e3ad1bc8
BeeProductive <beeproductiveapphelp@gmail.com>
2025-06-01 21:59:44 -0700

    scene del code

---
+++ src/IOSCodeSnippets/SceneDelegate.swift
@@ -0,0 +1,13 @@
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+     
+    }
